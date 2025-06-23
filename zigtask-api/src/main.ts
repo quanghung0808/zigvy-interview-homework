d8c27c8c1e37109f0412806d64fceedc17073b2a
Quang Hung <duongquanghung0122@gmail.com>
2025-06-23 22:38:23 +0700

    feat: add Swagger API documentation setup

--- zigtask-api/src/main.ts
+++ zigtask-api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './common/logging.interceptor';
 import { ResponseInterceptor } from './common/response.interceptor';
 import { AllExceptionsFilter } from './common/http-exception.filter';
@@ -11,8 +12,17 @@ async function bootstrap() {
     new LoggingInterceptor(),
     new ResponseInterceptor(),
   );
-
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('ZigTask API')
+    .setDescription('API documentation for ZigTask')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.enableCors();
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
