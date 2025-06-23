21e1b5bff265c4a4dcead57f5f3309915d09f3c5
Quang Hung <duongquanghung0122@gmail.com>
2025-06-23 22:36:41 +0700

    feat: add common module with global interceptors and filters

--- zigtask-api/src/main.ts
+++ zigtask-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/logging.interceptor';
+import { ResponseInterceptor } from './common/response.interceptor';
+import { AllExceptionsFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new ResponseInterceptor(),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
