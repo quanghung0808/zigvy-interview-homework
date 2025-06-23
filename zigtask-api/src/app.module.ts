7398c74db6cf36cf6be9ea244d9e8ee552939a32
Quang Hung <duongquanghung0122@gmail.com>
2025-06-24 01:57:55 +0700

    feat: register AuthModule to enable authentication features

--- zigtask-api/src/app.module.ts
+++ zigtask-api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.MONGO_URI || 'mongodb://localhost/zigtask',
     ),
+    AuthModule,
   ],
 })
 export class AppModule {}
