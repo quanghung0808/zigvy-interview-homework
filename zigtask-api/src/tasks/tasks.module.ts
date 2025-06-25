b26bb0e144daf1e61f2d2a862e8c119dd79e0ef9
Quang Hung <duongquanghung0122@gmail.com>
2025-06-25 16:43:48 +0700

    feat(tasks): register TaskGateway in TaskModule providers

--- zigtask-api/src/tasks/tasks.module.ts
+++ zigtask-api/src/tasks/tasks.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schema';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TaskGateway } from './task/task.gateway';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
   ],
-  providers: [TasksService],
+  providers: [TasksService, TaskGateway],
   controllers: [TasksController],
 })
 export class TasksModule {}
